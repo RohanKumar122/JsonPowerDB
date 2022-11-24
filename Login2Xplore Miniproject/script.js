45ea35401477ddb6b16bb25ce00ba955d7830aa4
Rohan Kumar <48582444+RohanKumar122@users.noreply.github.com>
2022-11-24 16:14:28 +0530

    Publish code to github

---
+++ Login2Xplore Miniproject/script.js
@@ -0,0 +1,102 @@
+$("#empId").focus();
+
+function validateAndGetFormData() {
+    var empIdVar = $("#empId").val();
+    if (empIdVar === "") {
+        alert("Employee ID Required Value");
+        $("#empId").focus();
+        return "";
+    }
+    var empNameVar = $("#empName").val();
+    if (empNameVar === "") {
+        alert("Employee Name is Required Value");
+        $("#empName").focus();
+        return "";
+    }
+    var empEmailVar = $("#empEmail").val();
+    if (empEmailVar === "") {
+        alert("Employee Email is Required Value");
+        $("#empEmail").focus();
+        return "";
+    }
+    var empMobileVar = $("#empMobile").val();
+    if (empMobileVar === "") {
+        alert("Employee Mobile is Required Value");
+        $("#empMobile").focus();
+        return "";
+    }
+    var jsonStrObj = {
+        empId: empIdVar,
+        empName: empNameVar,
+        empEmail: empEmailVar,
+        empMobile: empMobileVar
+    };
+    return JSON.stringify(jsonStrObj);
+}
+
+// This method is used to create PUT Json request.
+function createPUTRequest(connToken, jsonObj, dbName, relName) {
+    var putRequest =
+        "{\n" +
+        '"token" : "' +
+        connToken +
+        '",' +
+        '"dbName": "' +
+        dbName +
+        '",\n' +
+        '"cmd" : "PUT",\n' +
+        '"rel" : "' +
+        relName +
+        '",' +
+        '"jsonStr": \n' +
+        jsonObj +
+        "\n" +
+        "}";
+    return putRequest;
+}
+
+function executeCommand(reqString, dbBaseUrl, apiEndPointUrl) {
+    var url = dbBaseUrl + apiEndPointUrl;
+    var jsonObj;
+    $.post(url, reqString, function (result) {
+        jsonObj = JSON.parse(result);
+    }).fail
+
+        (function (result) {
+            var dataJsonObj = result.responseText;
+            jsonObj = JSON.parse(dataJsonObj);
+        });
+    return jsonObj;
+}
+
+function resetForm() {
+    $("#empId").val("");
+    $("#empName").val("");
+    $("#empEmail").val("");
+    $("#empMobile").val("");
+    $("#empId").focus();
+}
+
+function saveEmployee() {
+    var jsonStr = validateAndGetFormData();
+    if (jsonStr === "") {
+        return;
+    }
+    var putReqStr = createPUTRequest(
+        "90938005|-31949265621135057|90953240",
+        jsonStr,
+        "MINIPROJECT",
+        "EMP-REL"
+    );
+    alert(putReqStr);
+    jQuery.ajaxSetup({ async: false });
+    var resultObj = executeCommand(
+        putReqStr,
+        "http://api.login2explore.com:5577",
+        "/api/iml"
+    );
+    alert(JSON.stringify(resultObj));
+    jQuery.ajaxSetup({ async: true });
+    resetForm();
+    // alert("It works!!")
+}
